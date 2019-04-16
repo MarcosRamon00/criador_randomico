86a765cba0e6556b559bbf1b1d85644aad79e3bf
MarcosRamon00 <marcosramon2897@gmail.com>
2019-04-16 18:15:35 -0300

    refatoracao de geradorJogo.js

--- js/GeradorJogo.js
+++ js/GeradorJogo.js
@@ -14,27 +14,27 @@ function Jogo(){
     this.tema = this.setTema();
     this.jogabilidade = this.setJogabilidade();
 
+    function escolherCaracteristica(lista){//escolhe caracterista de uma lista
+        return Math.round(Math.random() * (lista.length - 1));
+    }
+
     function setEstilo(){
         let lista_estilo = ["labirinto","corrida sem fim","quebra cabeça","horror","FPS","ficção interativa","escape the room","Puzzle adventure","visual novel","RTS","RPG","JRPG","esporte","corrida","simulação","MMO","stealth"]; 
-        let escolha = Math.round(Math.random() * (lista_estilo.length - 1));
-        return lista_estilo[escolha];
+        return lista_estilo[escolherCaracteristica(lista_estilo)];
     }
 
     function setGraficos(){
         let lista_graficos = ["3D","2D","2.5D","texto","vetorial"];
-        let escolha = Math.round(Math.random() * (lista_graficos.length - 1));
-        return lista_graficos[escolha];
+        return lista_graficos[escolherCaracteristica(lista_graficos)];
     }
 
     function setTema(){
         let lista_tema = ["velho oeste","futurista","cyberpunk","diselpunk","pos apocaliptico"];
-        let escolha = Math.round(Math.random() * (lista_tema.length - 1));
-        return lista_tema[escolha];
+        return lista_tema[escolherCaracteristica(lista_tema)];
     }
 
     function setJogabilidade(){
         let lista_jogabilidade = ["shooter","MOBA","TURN BASED","luta","point and click","sandbox","open world","plataforma","hack and slash","beat and up","run and gun"];
-        let escolha = Math.round(Math.random() * (lista_jogabilidade.length - 1));
-        return lista_jogabilidade[escolha];
+        return lista_jogabilidade[escolherCaracteristica(lista_jogabilidade)];
     }
 }
--- js/GeradorPersonagem.js
+++ js/GeradorPersonagem.js
@@ -21,30 +21,22 @@ function Personagem(){
 
     function setRaca(){
         let lista_raca = ["humano","fera","demonio","anjo","ciborgue","robo","espirito","humano-réptil"]; 
-        //let escolha = Math.round(Math.random() * (lista_raca.length - 1));
-        //return lista_raca[escolha];
         return lista_raca[escolherCaracteristica(lista_raca)];
     }
 
     function setClasse(){
         let lista_classe = ["guerreiro(a)","mago(a)","bruxo(a)","paladino(a)","amazona","cantor",
     "medico(a)","sabio(a)","soldado(a)"];
-        //let escolha = Math.round(Math.random() * (lista_classe.length - 1));
-        //return lista_classe[escolha];
         return lista_classe[escolherCaracteristica(lista_classe)];
     }
 
     function setQualidade(){
         let lista_qualidade = ["sorte","força","inteligencia","resistencia","beleza","carisma"];
-       // let escolha = Math.round(Math.random() * (lista_qualidade.length - 1));
-        //return lista_qualidade[escolha];
         return lista_qualidade[escolherCaracteristica(lista_qualidade)];
     }
 
     function setDefeito(){
         let lista_defeito = ["azar","fraqueza fisica","ignorancia","feiura","bruto"];
-        //let escolha = Math.round(Math.random() * (lista_defeito.length - 1));
-        //return lista_defeito[escolha];
         return lista_defeito[escolherCaracteristica(lista_defeito)];
     }
 }
