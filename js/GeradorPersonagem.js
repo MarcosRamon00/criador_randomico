613db6352935cd85e5ae529aa56c3487d57a9e2c
Marcos Ramon <marcosramon2897@gmail.com>
2019-09-22 01:08:06 -0300

    adicao de racas

--- js/GeradorPersonagem.js
+++ js/GeradorPersonagem.js
@@ -9,13 +9,13 @@ const personagem = {
     },
 
     setRaca:function(){
-        let lista_raca = ["humano","fera","demonio","anjo","ciborgue","robo","espirito","humano-peixe","humano-passáro","humano-réptil","meio elemental","elemental","elemental do fogo","elemental da água","elemental das plantas","elemental do ar","elemental da terra","elemental do raio","elemental de gelo","elemental da luz","elemental da sombra","mutante","alienigina","fada","ogro","anão(a)","golem"];
+        let lista_raca = ["humano","fera","demonio","anjo","ciborgue","robo","espírito","humano-peixe","humano-passáro","humano-réptil","meio elemental","elemental","elemental do fogo","elemental da água","elemental das plantas","elemental do ar","elemental da terra","elemental do raio","elemental de gelo","elemental da luz","elemental da sombra","mutante","alienigina","fada","ogro","anão(a)","golem"];
         return lista_raca[this.escolherCaracteristica(lista_raca)];
     },
 
     setClasse:function(){
         let lista_classe = ["guerreiro(a)","mago(a)","bruxo(a)","paladino(a)","amazona","cantor(a)",
-    "médico(a)","sábio(a)","ferreiro(a)","bardo","caçador(a)","bruxo(a)","alquimista","lutador(a) de box","judoka","caçador(a) de recompensas","arqueiro(a)","lanceiro(a)"];
+    "médico(a)","sábio(a)","ferreiro(a)","bardo","caçador(a)","bruxo(a)","alquimista","lutador(a) de box","judoka","caçador(a) de recompensas","arqueiro(a)","lanceiro(a)","necromante","contador(a)"];
         return lista_classe[this.escolherCaracteristica(lista_classe)];
     },
 
