1e3bbcbe38f4d3b72cef1aeac0fa2ef0fda1cf5d
Marcos Ramon <marcosramon2897@gmail.com>
2019-10-05 03:19:36 -0300

    adicao de variavel idade

--- js/GerarPet.js
+++ js/GerarPet.js
@@ -1,5 +1,6 @@
 const pet = {
     especie :'',
+    idade :'',
 
     escolherCaracteristica:function(lista){//escolhe caracterista de uma lista
         return Math.round(Math.random() * (lista.length - 1));
@@ -9,8 +10,13 @@ const pet = {
         let lista_especies = ["gato","cachorro","rato","coruja","cobra","galinha"];
         return lista_especies[this.escolherCaracteristica(lista_especies)];
     },
+    setIdade:function(){
+        let lista_idades = ["filhote","adulto","idoso"];
+        return lista_idades[this.escolherCaracteristica(lista_idades)];
+    },
 
     gerar:function(){
         this.especie = this.setEspecie();
+        this.idade = this.setIdade();
     }
 }
