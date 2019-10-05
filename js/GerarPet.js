4e711fd050a3c244f8c234a942f798a3cf8c6827
Marcos Ramon <marcosramon2897@gmail.com>
2019-10-05 04:04:25 -0300

    adicionado comportamento a pet

--- js/GerarPet.js
+++ js/GerarPet.js
@@ -1,6 +1,7 @@
 const pet = {
     especie :'',
     idade :'',
+    comportamento:'',
 
     escolherCaracteristica:function(lista){//escolhe caracterista de uma lista
         return Math.round(Math.random() * (lista.length - 1));
@@ -14,9 +15,13 @@ const pet = {
         let lista_idades = ["filhote","adulto","idoso"];
         return lista_idades[this.escolherCaracteristica(lista_idades)];
     },
-
+    setComportamento:function(){
+        let lista_comportamentos = ["brincalhão(ona)","bravo(a)"];
+        return lista_comportamentos[this.escolherCaracteristica(lista_comportamentos)];
+    },
     gerar:function(){
         this.especie = this.setEspecie();
         this.idade = this.setIdade();
+        this.comportamento = this.setComportamento();
     }
 }
