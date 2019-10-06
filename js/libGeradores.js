43f38431606eb647bb627e068277cebf77a0caeb
Marcos Ramon <marcosramon2897@gmail.com>
2019-10-06 16:08:59 -0300

    refatoracao de ids

--- js/libGeradores.js
+++ js/libGeradores.js
@@ -7,31 +7,31 @@
  //personagem
  function gerarPersonagem(){//gerar personagem
      personagem.gerar();//gera personagem
-     document.getElementById("raca").innerHTML = personagem.raca;
-     document.getElementById("classe").innerHTML = personagem.classe;
-     document.getElementById("qualidade").innerHTML = personagem.qualidade;
-     document.getElementById("defeito").innerHTML = personagem.defeito;
-     document.getElementById("motivacao").innerHTML = personagem.motivacao;
+     document.getElementById("personagem_raca").innerHTML = personagem.raca;
+     document.getElementById("personagem_classe").innerHTML = personagem.classe;
+     document.getElementById("persoangem_qualidade").innerHTML = personagem.qualidade;
+     document.getElementById("personagem_defeito").innerHTML = personagem.defeito;
+     document.getElementById("personagem_motivacao").innerHTML = personagem.motivacao;
  }
  //jogo
  function gerarJogo(){//gerar jogo
      jogo.gerar();//gera jogo
-     document.getElementById("estilo").innerHTML = jogo.estilo;
-     document.getElementById("graficos").innerHTML = jogo.graficos;
-     document.getElementById("tema").innerHTML = jogo.tema;
-     document.getElementById("jogabilidade").innerHTML = jogo.jogabilidade;
+     document.getElementById("jogo_estilo").innerHTML = jogo.estilo;
+     document.getElementById("jogo_graficos").innerHTML = jogo.graficos;
+     document.getElementById("jogo_tema").innerHTML = jogo.tema;
+     document.getElementById("jogo_jogabilidade").innerHTML = jogo.jogabilidade;
  }
  //pet
  function gerarPet(){
      pet.gerar();
-     document.getElementById("especie").innerHTML = pet.especie;
-     document.getElementById("idade").innerHTML = pet.idade;
+     document.getElementById("pet_especie").innerHTML = pet.especie;
+     document.getElementById("pet_idade").innerHTML = pet.idade;
      document.getElementById("pet_comportamento").innerHTML = pet.comportamento;
  }
  //arma
  function gerarArma(){
      arma.gerar();
-     document.getElementById("arma").innerHTML = arma.arma;
-     document.getElementById("condicao").innerHTML = arma.condicao;
+     document.getElementById("arma_arma").innerHTML = arma.arma;
+     document.getElementById("arma_condicao").innerHTML = arma.condicao;
      document.getElementById("arma_estilo").innerHTML = arma.estilo;
  }
