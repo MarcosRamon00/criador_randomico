030d783520aef61838fba419dbe481025e3da1a6
Marcos Ramon <marcosramon2897@gmail.com>
2019-10-06 17:49:46 -0300

    adicionado tema noturno

---
+++ js/Temas/nocturnal.js
@@ -0,0 +1,36 @@
+const nocturnal = {
+    state : false,
+    
+    setEnableState:function(){
+        this.state = true;
+    },
+    setDisableState:function(){
+        this.state = false;
+    },
+    enable:function(){
+        document.body.style.background = "black";
+        document.body.style.color = "white";
+        document.getElementById("nav-personagem-tab").style.color = "white";
+        document.getElementById("nav-personagem-tab").style.background = "black";
+        document.getElementById("nav-jogo-tab").style.color = "white";
+        document.getElementById("nav-jogo-tab").style.background = "black";
+        document.getElementById("nav-pet-tab").style.color = "white";
+        document.getElementById("nav-pet-tab").style.background = "black";
+        document.getElementById("nav-arma-tab").style.color = "white";
+        document.getElementById("nav-arma-tab").style.background = "black";
+        this.setEnableState();
+    },
+    disable:function(){
+        document.body.style.background = "white";
+        document.body.style.color = "black";
+        document.getElementById("nav-personagem-tab").style.color = "black";
+        document.getElementById("nav-personagem-tab").style.background = "white";
+        document.getElementById("nav-jogo-tab").style.color = "black";
+        document.getElementById("nav-jogo-tab").style.background = "white";
+        document.getElementById("nav-pet-tab").style.color = "black";
+        document.getElementById("nav-pet-tab").style.background = "white";
+        document.getElementById("nav-arma-tab").style.color = "black";
+        document.getElementById("nav-arma-tab").style.background = "white";
+        this.setDisableState();
+    }
+}
