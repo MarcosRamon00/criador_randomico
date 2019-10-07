39c38bdb2307b9f473e24a7489ace8c74b9f3789
Marcos Ramon <marcosramon2897@gmail.com>
2019-10-07 18:58:37 -0300

    codigo traduzido para ingles

--- js/generatorsManager.js
+++ js/generatorsManager.js
@@ -4,34 +4,34 @@
  generatePet();
  generateWeapon();
  
- //personagem
- function generateCharacter(){//gerar personagem
-     personagem.gerar();//gera personagem
-     document.getElementById("personagem_raca").innerHTML = personagem.raca;
-     document.getElementById("personagem_classe").innerHTML = personagem.classe;
-     document.getElementById("personagem_qualidade").innerHTML = personagem.qualidade;
-     document.getElementById("personagem_defeito").innerHTML = personagem.defeito;
-     document.getElementById("personagem_motivacao").innerHTML = personagem.motivacao;
+ //character
+ function generateCharacter(){//gerar character
+     character.generate();//gera character
+     document.getElementById("character_raca").innerHTML = character.breed;
+     document.getElementById("character_classe").innerHTML = character.class;
+     document.getElementById("character_qualidade").innerHTML = character.quality;
+     document.getElementById("character_defeito").innerHTML = character.defect;
+     document.getElementById("character_motivacao").innerHTML = character.motivation;
  }
- //jogo
- function generateGame(){//gerar jogo
-     jogo.gerar();//gera jogo
-     document.getElementById("jogo_estilo").innerHTML = jogo.estilo;
-     document.getElementById("jogo_graficos").innerHTML = jogo.graficos;
-     document.getElementById("jogo_tema").innerHTML = jogo.tema;
-     document.getElementById("jogo_jogabilidade").innerHTML = jogo.jogabilidade;
+ //game
+ function generateGame(){//gerar game
+     game.generate();//gera game
+     document.getElementById("game_estilo").innerHTML = game.style;
+     document.getElementById("game_graficos").innerHTML = game.grafics;
+     document.getElementById("game_tema").innerHTML = game.theme;
+     document.getElementById("game_jogabilidade").innerHTML = game.gameplay;
  }
  //pet
  function generatePet(){
-     pet.gerar();
-     document.getElementById("pet_especie").innerHTML = pet.especie;
-     document.getElementById("pet_idade").innerHTML = pet.idade;
-     document.getElementById("pet_comportamento").innerHTML = pet.comportamento;
+     pet.generate();
+     document.getElementById("pet_especie").innerHTML = pet.species;
+     document.getElementById("pet_idade").innerHTML = pet.age;
+     document.getElementById("pet_comportamento").innerHTML = pet.behavior;
  }
- //arma
+ //weapon
  function generateWeapon(){
-     arma.gerar();
-     document.getElementById("arma_arma").innerHTML = arma.arma;
-     document.getElementById("arma_condicao").innerHTML = arma.condicao;
-     document.getElementById("arma_estilo").innerHTML = arma.estilo;
+     weapon.generate();
+     document.getElementById("weapon_weapon").innerHTML = weapon.weapon;
+     document.getElementById("weapon_condicao").innerHTML = weapon.condition;
+     document.getElementById("weapon_estilo").innerHTML = weapon.style;
  }
